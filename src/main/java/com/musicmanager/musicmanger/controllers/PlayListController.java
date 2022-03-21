a48a3fb2ebcd28dbf3b9e6f0933565dde3435469
batrinhbrt11 <trinhnguyenleba@gmail.com>
2022-03-21 10:08:44 +0700

    finall

--- src/main/java/com/musicmanager/musicmanger/controllers/PlayListController.java
+++ src/main/java/com/musicmanager/musicmanger/controllers/PlayListController.java
@@ -33,6 +33,7 @@ public class PlayListController {
             return null;
         });
         if(updatedMusic != null){
+            
             return  updatedMusic;
         }else{
             return null;
